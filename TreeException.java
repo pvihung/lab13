c396489d9b5ae9081e08da90e287b954f08a8810
pvihung <hung.v.pham@sjsu.edu>
2024-04-26 17:36:10 -0700

    first commit

---
+++ TreeException.java
@@ -0,0 +1,11 @@
+package trees;
+
+import java.io.IOException;
+
+public class TreeException extends FamilyTree {
+
+	public TreeException() throws IOException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+}
